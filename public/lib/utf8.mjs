6c7e04f9637e07746bcadd4b1090eefc3dbca1be
metastable-void <git@yuka.h09.eu>
2021-04-11 00:43:14 +0900

    removed unused codes

--- public/lib/utf8.mjs
+++
@@ -1,6 +0,0 @@
-
-const textEncoder = new TextEncoder;
-const textDecoder = new TextDecoder;
-
-export const encode = str => textEncoder.encode(str);
-export const decode = bytes => textDecoder.decode(bytes);
